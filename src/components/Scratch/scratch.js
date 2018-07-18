86a357a5d55cfa7c9e46688453b24e7494df7280
Dameon1 <dameonmedoza@gmail.com>
2018-07-18 13:00:34 -0700

    insert Repl.it

--- src/components/Scratch/scratch.js
+++ src/components/Scratch/scratch.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import YouTube from 'react-youtube';
- 
+let videos = require('./scratchVideoObjects');
+
+console.log(videos);
 export default class Scratch extends React.Component {
   render() {
     const opts = {
       height: '390',
       width: '640',
       playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 1
+        autoplay: 1,
+        'origin':'http://localhost:3000'
       }
     };
  
     return (
+      <div>
       <YouTube
-        videoId="2g811Eo7K8U"
+        videoId={videos[2].id}
         opts={opts}
+        host='http://localhost:3000'
         onReady={this._onReady}
+        onEnd={()=>console.log("Go func yourself")} 
       />
+      <iframe title='Repl.it' frameBorder="0" width="100%" height="500px" src="https://repl.it/Dn6S?lite=true"></iframe>
+      </div>
     );
   }
  
   _onReady(event) {
     // access to player in all event handlers via event.target
-    event.target.pauseVideo();
+    event.target.playVideo();
   }
 }
---
+++ src/components/Scratch/scratchVideoObjects.js
@@ -0,0 +1,23 @@
+
+let videos = [
+  {
+    length:'1:00:44',
+    id:'UB1O30fR-EE',
+  },
+  
+  {
+    length:'8:08',
+    id:'h_3nR-Huxoo',
+  },
+  
+  {
+    length:'5:48',
+    id:'bCjWAI2lC20',
+  }
+];
+
+module.exports = videos;
+  
+
+
+
