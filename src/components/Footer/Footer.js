6e96ae473b00e13618c89b1b9fe64e7252be7b90
Terrance Corley <terrancecorley@outlook.com>
2018-07-19 09:02:55 -0700

    refactored foot text

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>
+          &copy; 2018 Made possible by <a href="https://github.com/jimmlusk" target="_blank" rel="noopener noreferrer">Jimm</a>, <a href="https://github.com/Dameon1" target="_blank" rel="noopener noreferrer">Dameon</a>, <a href="https://github.com/arsalonk" target="_blank" rel="noopener noreferrer">Sayed</a>, &amp; <a href="https://terrancecorley.com" target="_blank" rel="noopener noreferrer">Terrance</a>.
+        </p>
+      </footer>
+    );
+  }
+}
