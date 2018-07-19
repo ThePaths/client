92c082eea6ec99699d2e06c9342aabddb5208fd9
Sayed Khan <arsalon47@gmail.com>
2018-07-19 17:29:53 -0400

    added files to user landingpage

---
+++ src/components/LandingPages/LandingPageUser/explore.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Explore(props) {
+  return (
+    <div>
+      <p>will display all paths</p>
+    </div>
+  )
+}
---
+++ src/components/LandingPages/LandingPageUser/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home(props) {
+  return (
+    <div>
+      <p>will display user path progress</p>
+    </div>
+  )
+}
