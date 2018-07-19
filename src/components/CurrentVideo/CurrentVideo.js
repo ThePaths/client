e0cacabbe1306830b9f738772dd65a8e138ebfaf
Terrance Corley <terrancecorley@outlook.com>
2018-07-19 09:01:42 -0700

    added footer component

--- src/components/CurrentVideo/CurrentVideo.js
+++ src/components/CurrentVideo/CurrentVideo.js
@@ -7,12 +7,12 @@ export default class CurrentVideo extends Component {
   render() {
     return (
       <YouTube
-      videoId='bCjWAI2lC20'
-      opts={opts}
-      host='http://localhost:3000'
-      onReady={this._onReady}
-      onEnd={()=>console.log("Go func yourself")} 
-    />
+        videoId='bCjWAI2lC20'
+        opts={opts}
+        host='http://localhost:3000'
+        onReady={this._onReady}
+        onEnd={()=>console.log('Go func yourself')} 
+      />
     );
   }
 }
--- src/components/Scratch/scratch.js
+++ src/components/Scratch/scratch.js
@@ -9,21 +9,21 @@ console.log(videos);
 export default class Scratch extends React.Component {
   constructor(props){
     super(props);
-      this.state = {
-        currentVideoIndex:0
-        }
-      }
+    this.state = {
+      currentVideoIndex:0
+    };
+  }
   
   buttonClickHandler(){
     console.log('buttonClicked');
     if(this.state.currentVideoIndex === 2){
-    return  this.setState({
+      return  this.setState({
         currentVideoIndex: 0
-      })
+      });
     }
     this.setState({
       currentVideoIndex: this.state.currentVideoIndex + 1
-    })
+    });
   }    
   
 
@@ -41,19 +41,19 @@ export default class Scratch extends React.Component {
     return (
       <div>
        
-      <div className='scratch'>
+        <div className='scratch'>
      
-      <YouTube className="youtubePlayer"
-        videoId={videos[this.state.currentVideoIndex].id}
-        opts={opts}
-        host='http://localhost:3000'
-        onReady={this._onReady}
-        onEnd={()=>this.buttonClickHandler()} 
-      />
+          <YouTube className="youtubePlayer"
+            videoId={videos[this.state.currentVideoIndex].id}
+            opts={opts}
+            host='http://localhost:3000'
+            onReady={this._onReady}
+            onEnd={()=>this.buttonClickHandler()} 
+          />
       
      
-      <div>
-      <iframe className="replItIframe"
+          <div>
+            <iframe className="replItIframe"
               title="firstAttempt"
               height="400px" 
               width="100%" 
@@ -65,12 +65,12 @@ export default class Scratch extends React.Component {
               allowFullScreen="true" 
               
               sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals">
-      </iframe>
-      </div>
+            </iframe>
+          </div>
       
+        </div>
+        <button onClick={()=>this.buttonClickHandler()}>Button</button>
       </div>
-      <button onClick={()=>this.buttonClickHandler()}>Button</button>
-     </div>
         
      
     );
