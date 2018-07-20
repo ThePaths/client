c05a8046d3f4360e92a74e94fddd7a72c97f33eb
Terrance Corley <terrancecorley@outlook.com>
2018-07-19 21:37:57 -0700

    testing push

--- src/components/Repl.it/Repl.js
+++ src/components/Repl.it/Repl.js
@@ -3,19 +3,21 @@ import './Repl.css';
 const videos = require('../Scratch/scratchVideoObjects');
 // testing for commit
 
-export default function Repl() {
-  return (
-    <iframe className="repl"
-      title="firstAttempt"
-      height="400px" 
-      width="100%" 
-      //=======================Connect this line with state==================================
-      src={videos[0].replit} 
-      scrolling="no" 
-      frameBorder="no"
-      allowtransparency="true" 
-      allowFullScreen="true" 
-      sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals">
-    </iframe>
-  );
+export default class Repl extends React.Component {
+  render() {
+    return (
+      <iframe className="repl"
+        title="firstAttempt"
+        height="400px" 
+        width="100%" 
+        //=======================Connect this line with state==================================
+        src={videos[0].replit} 
+        scrolling="no" 
+        frameBorder="no"
+        allowtransparency="true" 
+        allowFullScreen="true" 
+        sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals">
+      </iframe>
+    );
+  }
 }
