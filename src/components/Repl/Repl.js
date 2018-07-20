33646dac1db2479936a746cfcf84814bfb0ed7e6
Dameon1 <dameonmedoza@gmail.com>
2018-07-20 15:23:53 -0700

    made Repl dynamic

--- src/components/Repl/Repl.js
+++ src/components/Repl/Repl.js
@@ -11,7 +11,7 @@ export default class Repl extends React.Component {
         height="400px" 
         width="100%" 
         //=======================Connect this line with state==================================
-        src={videos[0].replit} 
+        src={this.props.repl[index]} 
         scrolling="no" 
         frameBorder="no"
         allowtransparency="true" 
@@ -21,3 +21,10 @@ export default class Repl extends React.Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  repl: state.auth.currentUser.displayPath.videos || null,
+  index: state.auth.currentUser.displayPath.index || null
+});
+
+export default connect(mapStateToProps)(LandingPage);
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -14,35 +14,6 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
-<<<<<<< HEAD
-    if (action.type === SET_AUTH_TOKEN) {
-        return Object.assign({}, state, {
-            authToken: action.authToken
-        });
-    } else if (action.type === CLEAR_AUTH) {
-        return Object.assign({}, state, {
-            authToken: null,
-            currentUser: null
-        });
-    } else if (action.type === AUTH_REQUEST) {
-        return Object.assign({}, state, {
-            loading: true,
-            error: null
-        });
-    } else if (action.type === AUTH_SUCCESS) {
-        console.log('current user: ', action.currentUser)
-        return Object.assign({}, state, {
-            loading: false,
-            currentUser: action.currentUser
-        });
-    } else if (action.type === AUTH_ERROR) {
-        return Object.assign({}, state, {
-            loading: false,
-            error: action.error
-        });
-    }
-    return state;
-=======
   if (action.type === SET_AUTH_TOKEN) {
     return Object.assign({}, state, {
       authToken: action.authToken
@@ -69,5 +40,4 @@ export default function reducer(state = initialState, action) {
     });
   }
   return state;
->>>>>>> ae019f7d89535c5aadcb16b6cf4ba2b087dd199d
 }
