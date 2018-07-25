416b80a224db3cefc1616f095b47dd807461ecf6
Terrance Corley <terrancecorley@outlook.com>
2018-07-25 09:38:50 -0700

    testing header config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 
-export const MAIN_HEADER_URL = process.env.MAIN_HEADER_URL || 'http://localhost:3000/';
+export const MAIN_HEADER_URL = process.env.REACT_APP_MAIN_HEADER_URL || 'http://localhost:3000/';
 
-export const AUTH_HEADER_URL = process.env.AUTH_HEADER_URL || 'http://localhost:3000/auth';
+export const AUTH_HEADER_URL = process.env.REACT_APP_AUTH_HEADER_URL || 'http://localhost:3000/auth';
