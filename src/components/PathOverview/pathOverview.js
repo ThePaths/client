10b58341674508aaa72a5de4c079e207f782e589
Terrance Corley <terrancecorley@outlook.com>
2018-07-25 11:31:44 -0700

    began path overview refacotr

--- src/components/PathOverview/pathOverview.js
+++ src/components/PathOverview/pathOverview.js
@@ -1,44 +1,51 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUserPaths } from '../../actions/userPaths';
 
 export class PathOverview extends React.Component {
 
   componentDidMount() {
-    this.props.dispatch(fetchUserPaths())
+    this.props.dispatch(fetchUserPaths());
   }
 
   render() {
-    if(!this.props.loading) {
-    const videos = this.props.path.videos.map((video, index) => {
-      return (
-        <li key={index}>
-          <h2>{this.props.path.title}</h2>
-          <p>{this.props.path.description}</p>
-          <img src={`http://img.youtube.com/vi/${video.videoId}/1.jpg`} alt='FIX' 
-            onClick={() => window.location.href = '/classroom'}
-          />
-         
-        </li>
-      )
-    })
     return (
-      <div>
-        <h2>{this.props.path.title}</h2>
-         <p>{this.props.path.description}</p>
-        <ul>{videos}</ul>
-      </div>
-    )
-  }
-  return (
-    null
-  )
+      <p>Hai</p>
+    );
   }
 }
 
 const mapStateToProps = state => ({
   path: state.userPaths.userPaths.displayPath,
   loading: state.userPaths.loading
-})
+});
 
 export default connect(mapStateToProps)(PathOverview);
+
+
+
+// reference code
+// if(!this.props.loading) {
+//   const videos = this.props.path.videos.map((video, index) => {
+//     return (
+//       <li key={index}>
+//         <h2>{this.props.path.title}</h2>
+//         <p>{this.props.path.description}</p>
+//         <img src={`http://img.youtube.com/vi/${video.videoId}/1.jpg`} alt='FIX' 
+//           onClick={() => window.location.href = '/classroom'}
+//         />
+     
+//       </li>
+//     );
+//   });
+//   return (
+//     <div>
+//       <h2>{this.props.path.title}</h2>
+//       <p>{this.props.path.description}</p>
+//       <ul>{videos}</ul>
+//     </div>
+//   );
+// }
+// return (
+//   null
+// );
