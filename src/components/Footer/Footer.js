d47936462a674298e2325bc09ec29871ff982ef4
Terrance Corley <terrancecorley@outlook.com>
2018-07-30 09:36:30 -0700

    added path overview checkmark func

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,21 +7,21 @@ export default class Footer extends React.Component {
       <footer className="main-footer">
         <p>
           &copy; 2018 Created by <a className="site-creator-link" 
-                                    href="https://github.com/jimmlusk" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer">Jimm</a>, 
-                                 <a className="site-creator-link" 
-                                    href="https://github.com/Dameon1" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer">Dameon</a>, 
-                                 <a className="site-creator-link" 
-                                    href="https://github.com/arsalonk" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer">Sayed</a>, &amp;
-                                 <a className="site-creator-link" 
-                                    href="https://terrancecorley.com" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer">Terrance</a>.
+            href="https://github.com/jimmlusk" 
+            target="_blank" 
+            rel="noopener noreferrer">Jimm</a>, 
+          <a className="site-creator-link" 
+            href="https://github.com/Dameon1" 
+            target="_blank" 
+            rel="noopener noreferrer">Dameon</a>, 
+          <a className="site-creator-link" 
+            href="https://github.com/arsalonk" 
+            target="_blank" 
+            rel="noopener noreferrer">Sayed</a>, &amp;
+          <a className="site-creator-link" 
+            href="https://terrancecorley.com" 
+            target="_blank" 
+            rel="noopener noreferrer">Terrance</a>.
         </p>
       </footer>
     );
