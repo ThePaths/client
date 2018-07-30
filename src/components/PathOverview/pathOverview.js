01561b5cbee1e80d4227bae0339e2096f6839154
Sayed Khan <arsalon47@gmail.com>
2018-07-30 16:13:58 -0400

    fixed go so it removes path from saved

--- src/components/PathOverview/pathOverview.js
+++ src/components/PathOverview/pathOverview.js
@@ -74,7 +74,10 @@ export class PathOverview extends React.Component {
               {/* add link to video's classroom */}
               <Link 
                 onClick={() => {
-                  if (this.props.status !== 'current' && this.props.status !== 'completed') {
+                  if (this.props.status === 'saved') {
+                    this.removeFromSaved()
+                    this.props.dispatch(addToUserCurrent(this.props.path.id));
+                  } else if (this.props.status !== 'current' && this.props.status !== 'completed') {
                     this.props.dispatch(addToUserCurrent(this.props.path.id));
                   }
                 }}
