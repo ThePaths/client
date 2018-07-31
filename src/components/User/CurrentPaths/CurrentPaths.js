aff1c653b7ae34b16ac0be4a25475920343857d3
Terrance Corley <terrancecorley@outlook.com>
2018-07-31 14:31:57 -0700

    added cloudinary links to landing and dashboard

--- src/components/User/CurrentPaths/CurrentPaths.js
+++ src/components/User/CurrentPaths/CurrentPaths.js
@@ -6,33 +6,33 @@ export function CurrentPaths(props) {
   if (props.current.length > 0) {
     currentPath = props.current.map((path, index) => {
       return (
-        <li key={index} onClick={ () => {window.location.href = `/dashboard/overview/${path.path.id}`} }>
+        <li key={index} onClick={ () => {window.location.href = `/dashboard/overview/${path.path.id}`;} }>
           <p>{path.path.title}</p>
-          <img src={path.path.hero} alt='' className="heroImage" />
+          <img src={`https://res.cloudinary.com/thepaths/image/upload/v1533069112/thumbnails/${path.path.id}`} alt='' className="heroImage" />
         </li>
-      )
-    })
+      );
+    });
   } else {
     currentPath = <li>
       <p>You are currently following no paths, go to explore to find some</p>
       <button className="currentPathsBUtton"
         onClick={() => window.location.href = '/dashboard/explore'}>Explore</button>
         
-    </li>
+    </li>;
   }
 
   return (
     <div className="currentPathsContainer">
-    <h2>Keep Learning</h2>
+      <h2>Keep Learning</h2>
       <ul>
         {currentPath}
       </ul>
     </div>
-  )
+  );
 }
 
 const mapStateToProps = state => ({
   current: state.userPaths.current || 0
-})
+});
 
-export default connect(mapStateToProps)(CurrentPaths)
+export default connect(mapStateToProps)(CurrentPaths);
