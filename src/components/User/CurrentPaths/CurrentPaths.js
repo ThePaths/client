e29634e0e470e5c2b43878a0b650ef8bcf8999ae
Dameon1 <dameonmedoza@gmail.com>
2018-07-31 15:19:50 -0700

    currentPaths updated for LINK

--- src/components/User/CurrentPaths/CurrentPaths.js
+++ src/components/User/CurrentPaths/CurrentPaths.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
 import './currentPaths.css';
 export function CurrentPaths(props) {
   let currentPath;
   if (props.current.length > 0) {
     currentPath = props.current.map((path, index) => {
-      return (
-        <li key={index} onClick={ () => {window.location.href = `/dashboard/overview/${path.path.id}`} }>
+      return (<li>
+        <Link key={index} to={`/dashboard/overview/${path.path.id}`}>
           <p>{path.path.title}</p>
           <img src={path.path.hero} alt='' className="heroImage" />
+        </Link>
         </li>
       )
     })
   } else {
     currentPath = <li>
       <p>You are currently following no paths, go to explore to find some</p>
-      <button className="currentPathsBUtton"
-        onClick={() => window.location.href = '/dashboard/explore'}>Explore</button>
-        
+      <Link to={'/dashboard/explore'}><button className="currentPathsBUtton"
+        >Explore</button>
+       </Link> 
     </li>
   }
 
@@ -35,4 +37,4 @@ const mapStateToProps = state => ({
   current: state.userPaths.current || 0
 })
 
-export default connect(mapStateToProps)(CurrentPaths)
+export default withRouter(connect(mapStateToProps)(CurrentPaths))
