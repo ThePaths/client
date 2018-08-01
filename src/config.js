e72ef8c8b4abf23d95a75c5675d270548bf5ad9c
unknown <ajameslusk@gmail.com>
2018-08-01 17:36:40 -0400

    login/register styling to match new look

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080' || 'http://localhost:5000';
