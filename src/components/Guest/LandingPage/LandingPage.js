a159421a6515445a1d15ebae4bf166dec114f070
Dameon Mendoza <dameonmendoza@gmail.com>
2021-06-18 18:44:08 -0700

    removed pictures on load

--- src/components/Guest/LandingPage/LandingPage.js
+++ src/components/Guest/LandingPage/LandingPage.js
@@ -14,21 +14,7 @@ export class LandingPage extends React.Component {
     if (this.props.loggedIn) {
       return <Redirect to="/dashboard" />;
     }
-    if (!this.props.loading) {
-      const jimm =
-        "https://media.licdn.com/dms/image/C4D03AQE5aRtIPHO6HQ/profile-displayphoto-shrink_800_800/0?e=1537401600&v=beta&t=kUHFHcnLAoWfegX1JYRQG9ZQP4RK3tYZU3htMo1yPqc";
-      const sayed =
-        "https://media.licdn.com/dms/image/C4D03AQHn-tjeDHXPVg/profile-displayphoto-shrink_800_800/0?e=1537401600&v=beta&t=i4O7yXaUUx1TY0GtcOSR-_nwW5gNfIhQ_xCYZV3CXJE";
-      const dameon =
-        "https://media.licdn.com/dms/image/C5603AQE56jBytxJMPA/profile-displayphoto-shrink_800_800/0?e=1537401600&v=beta&t=kTtfTglP1E9jjdpk5mXLGla8dY1wmuKJEtnCN5KQrnY";
-      const terrance =
-        "https://avatars2.githubusercontent.com/u/20272233?s=460&v=4";
-      console.log("The Four Creators of The Paths");
-      console.image(jimm);
-      console.image(sayed);
-      console.image(dameon);
-      console.image(terrance);
-    }
+   
     const paths = this.props.paths.map((path, index) => {
       return (
         <li
@@ -40,7 +26,7 @@ export class LandingPage extends React.Component {
             src={`https://res.cloudinary.com/thepaths/image/upload/v1533069112/thumbnails/${
               path.id
             }.png`}
-            alt={`image of ${path.title}`}
+            alt={`${path.title}`}
             className="heroImage"
           />
           <h2 className="sample-path-title">{path.title}</h2>
